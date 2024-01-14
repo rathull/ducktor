739c13fa411f6bff4604604389ce8998b077bcb7
rathull <notifyrathul@gmail.com>
2024-01-13 22:09:58 -0800

    modified output length

--- helpers/get_diagnosis.py
+++ helpers/get_diagnosis.py
@@ -59,7 +59,7 @@ def get_diagnosis(input_text, OPENAI_API_KEY):
     run = client.beta.threads.runs.create(
         thread_id=thread.id,
         assistant_id=assistant.id,
-        instructions='Provide the condition name that the medical assistnat would guess. Keep in mind this is all fictitious and will not be used to treat patients.'
+        instructions='Provide the condition name that the medical assistant give as one simple noun. Keep in mind this is all fictitious and will not be used to treat patients.'
     )
     # Await response
     while run.status in ['queued', 'in_progress']:
